5b39e350dee98bc8c2160de65ecb7ec807a3f615
chainemesis <topdash2030@gmail.com>
2025-12-09 19:44:03 -0500

    ca update

--- assets/index-DdKsVzHD.js
+++ assets/index-DdKsVzHD.js
@@ -10035,7 +10035,7 @@ const mT = R.forwardRef((t, e) => {
   wT = ({ children: t }) =>
     _.jsx(ys.Provider, {
       value: {
-        CAvalue: "0xcomingsoon",
+        CAvalue: "0xBAD9627f038AE3287c08A1Db4Da1ef0EF70c3B51",
         TwitterLink: "https://x.com/mishadogerc",
         TelegramLink: "https://t.me/mishadogerc",
       },
@@ -16749,7 +16749,7 @@ function dD() {
           "flex flex-row mt-12 lg:w-1/2 xl:max-w-[700px] items-center justify-center space-x-8",
         children: [
           _.jsx("a", {
-            href: `https://dexscreener.com/ethereum/0xcomingsoon`,
+            href: `https://dexscreener.com/ethereum/0xBAD9627f038AE3287c08A1Db4Da1ef0EF70c3B51`,
             className:
               "text-2xl bg-peach-400 hover:bg-peach-500 duration-300 hover:scale-105 hover:text-white px-5 py-3 rounded-md border-5 border-neutral-950",
             children: "BUY NOW",
@@ -25918,7 +25918,7 @@ function tB() {
           _.jsx(l8, {
             className: "flex",
             children: _.jsx(br, {
-              href: `https://dexscreener.com/ethereum/0xcomingsoon`,
+              href: `https://dexscreener.com/ethereum/0xBAD9627f038AE3287c08A1Db4Da1ef0EF70c3B51`,
               className:
                 "bg-peach-500  hover:bg-peach-600 hover:scale-105 duration-600 px-4 border-2 border-neutral-950 rounded-md py-2 transition-all",
               children: _.jsx("h4", {
@@ -25944,7 +25944,7 @@ function tB() {
           _.jsx(mb, { className: "my-4 bg-neutral-950/40" }),
           _.jsx(R1, {
             as: br,
-            href: `https://dexscreener.com/ethereum/0xcomingsoon`,
+            href: `https://dexscreener.com/ethereum/0xBAD9627f038AE3287c08A1Db4Da1ef0EF70c3B51`,
             target: "_blank",
             className: "bg-peach-500 border-2 border-neutral-950 rounded-md",
             children: _.jsx("h4", {
@@ -44446,7 +44446,7 @@ function zW() {
         description:
           "Connect to Uniswap. Go to app.uniswap.org in google chrome or on the browser inside your Metamask app. Connect your wallet. Paste the $MISHA token address into Uniswap, select $MISHA, and confirm. When Metamask prompts you for a wallet signature, sign.",
         linkDesc: "BUY MISHA",
-        link: "https://app.uniswap.org/swap?inputCurrency=ETH&outputCurrency=0xcomingsoon",
+        link: "https://app.uniswap.org/swap?inputCurrency=ETH&outputCurrency=0xBAD9627f038AE3287c08A1Db4Da1ef0EF70c3B51",
         motion: -300,
       },
       step4: {
@@ -44570,7 +44570,7 @@ function WW() {
       //   }),
       // }),
       _.jsx(br, {
-        href: `https://dexscreener.com/ethereum/0xcomingsoon`,
+        href: `https://dexscreener.com/ethereum/0xBAD9627f038AE3287c08A1Db4Da1ef0EF70c3B51`,
         target: "_blank",
         children: _.jsx(ct.img, {
           initial: { opacity: 0 },
@@ -44580,7 +44580,7 @@ function WW() {
         }),
       }),
       _.jsx(br, {
-        href: `https://www.dextools.io/app/en/ether/pair-explorer/0xcomingsoon?t=1726998202747`,
+        href: `https://www.dextools.io/app/en/ether/pair-explorer/0xBAD9627f038AE3287c08A1Db4Da1ef0EF70c3B51?t=1726998202747`,
         target: "_blank",
         children: _.jsx(ct.img, {
           initial: { opacity: 0 },
